9f7e7fbaae7bcbe74715ea325ac6de3124db18b2
Brian Ray <bray_stenovations@yahoo.com>
2025-02-05 12:32:44 -0500

    Add project files.

---
+++ OnGetDemo/Pages/Privacy.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OnGetDemo.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        [BindProperty(SupportsGet = true)]
+        public int Param1 { get; set; } = 0;
+
+        [BindProperty(SupportsGet = true)]
+        public int Param2 { get; set; } = 0;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+
+}
